ca405eb27edbd35574e8a0690b5be3b726bb85cc
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2023-03-03 22:17:23 -0500

    add base pages

---
+++ dashboard/announcements/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require "../incl/dashboardLib.php";
+require "../".$dbPath."incl/lib/connection.php";
+$dl = new dashboardLib();
+require_once "../".$dbPath."incl/lib/mainLib.php";
+require "../".$dbPath."incl/lib/exploitPatch.php";
+error_reporting(0);
+$gs = new mainLib();
+include "../".$dbPath."incl/lib/connection.php";
+$dl->title($dl->getLocalizedString("announcements"));
+$dl->printFooter('../');
+?>
