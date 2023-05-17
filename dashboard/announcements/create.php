923231b85ee7b216176d1bc4e4b8349e9e840678
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2023-05-17 15:57:29 -0400

    add mod only

--- dashboard/announcements/create.php
+++ dashboard/announcements/create.php
@@ -1,4 +1,5 @@
 <?php
+try {
 session_start();
 require "../incl/dashboardLib.php";
 require "../".$dbPath."incl/lib/connection.php";
@@ -8,8 +9,39 @@ require "../".$dbPath."incl/lib/exploitPatch.php";
 error_reporting(0);
 $gs = new mainLib();
 include "../".$dbPath."incl/lib/connection.php";
-$dl->title($dl->getLocalizedString("createAnnouncement"));
 $dl->printFooter('../');
+if((!isset($_SESSION["accountID"]) AND $_SESSION["accountID"] != 0)){
+  $dl->printSong('<div class="form">
+    <h1>'.$dl->getLocalizedString("errorGeneric").'</h1>
+	<p id="bruh">'.$dl->getLocalizedString("noPermission").'</p>
+    <form class="form__inner" method="post" action=".">
+	<button type="button" onclick="a(\'\')" class="btn-primary">'.$dl->getLocalizedString("Kish!").'</button>
+    </form>
+	</div>', 'mod');
+	die();
+}
+if(!isset($_SESSION["accountID"]) | !$gs->checkPermission($_SESSION["accountID"], 'announcementCreate')) {
+	$dl->printSong('<div class="form">
+    <h1>'.$dl->getLocalizedString("errorGeneric").'</h1>
+	<p id="bruh">'.$dl->getLocalizedString("noPermission").'</p>
+    <form class="form__inner" method="post" action=".">
+	<button type="button" onclick="a(\'\')" class="btn-primary">'.$dl->getLocalizedString("Kish!").'</button>
+    </form>
+	</div>', 'mod');
+	die();
+}
+} catch (Exception $e) {
+  echo '<script> console.log("Error: ' . $e->getMessage() . '")</script>';
+  $dl->printSong('<div class="form">
+    <h1>'.$dl->getLocalizedString("errorGeneric").'</h1>
+	<p id="bruh">'.$dl->getLocalizedString("noPermission").'</p>
+    <form class="form__inner" method="post" action=".">
+	<button type="button" onclick="a(\'\')" class="btn-primary">'.$dl->getLocalizedString("Kish!").'</button>
+    </form>
+	</div>', 'mod');
+	die();
+}
+$dl->title($dl->getLocalizedString("createAnnouncement"));
 $dl->printSong("<div class='form'>
   <h1>".$dl->getLocalizedString("createAnnouncement")."</h1>
   <p>Not finished yet.</p>
