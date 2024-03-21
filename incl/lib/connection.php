47c3069fe139be14740a2aa7aa4f1288cedefae3
M336 <75208577+M336G@users.noreply.github.com>
2024-03-21 18:59:33 +0100

    Update connection.php

--- incl/lib/connection.php
+++ incl/lib/connection.php
@@ -3,6 +3,7 @@ error_reporting(0);
 include dirname(__FILE__)."/../../config/connection.php";
 @header('Content-Type: text/html; charset=utf-8');
 
+// Combine all of https://github.com/SevenworksDev/proxy-list/tree/main/proxies as /database/config/proxies.txt
 if (in_array($_SERVER['REMOTE_ADDR'], file('../../config/proxies.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES))) {
     http_response_code(404);
     exit;
