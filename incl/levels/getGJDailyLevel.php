9d38fdff657cd73ac1ff6be9e86a0e6907caaae2
M336 <75208577+M336G@users.noreply.github.com>
2024-06-30 01:25:44 +0200

    Add the correct daily IDs toggle check

--- incl/levels/getGJDailyLevel.php
+++ incl/levels/getGJDailyLevel.php
@@ -2,12 +2,18 @@
 chdir(dirname(__FILE__));
 include "../lib/connection.php";
 include "../lib/mainLib.php";
+include_once "../../config/misc.php";
 $gs = new mainLib();
 $type = !empty($_POST["type"]) ? $_POST["type"] : (!empty($_POST["weekly"]) ? $_POST["weekly"] : 0);
 $midnight = ($type == 1) ? strtotime("next monday") : strtotime("tomorrow 00:00:00");
 $current = time();
-$query = $db->prepare("SELECT * FROM dailyfeatures WHERE timestamp < :current AND type = :type ORDER BY timestamp DESC LIMIT 1");
-$query->execute([':current' => $current, ':type' => $type]);
+if(!correctDailysIDs) {
+	$query = $db->prepare("SELECT * FROM dailyfeatures WHERE timestamp < :current AND type = :type ORDER BY timestamp DESC LIMIT 1");
+	$query->execute([':current' => $current, ':type' => $type]);
+} else {
+	$query=$db->prepare("SELECT COUNT(levelID) FROM dailyfeatures WHERE type = :type");
+	$query->execute([':type' => $type]);
+}
 $daily = $query->fetch();
 if($query->rowCount() == 0) exit("-1");
 $dailyID = $daily['feaID'];
