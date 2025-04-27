c2395dce19f271bffa6321536918d8d3339c2a4e
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-04-27 20:23:31 +0100

    fix attempt 2

--- incl/clans/getClans.php
+++ incl/clans/getClans.php
@@ -3,25 +3,32 @@ chdir(dirname(__FILE__));
 require "../lib/connection.php";
 
 $query = "SELECT * FROM clans";
-
 $result = $db->query($query);
 
-$response = array();
+if ($result) {
+    if ($result->num_rows > 0) {
+        $response = array();
 
-while ($row = $result->fetch_assoc()) {
-    $clan = array(
-        'ID' => $row['ID'],
-        'name' => $row['clan'],
-        'tag' => $row['tag'],
-        'desc' => $row['desc'],
-        'clanOwner' => $row['clanOwner'],
-        'color' => $row['color'],
-        'isClosed' => $row['isClosed'],
-        'creationDate' => $row['creationDate']
-    );
-    
-    $response[] = $clan;
+        while ($row = $result->fetch_assoc()) {
+            $clan = array(
+                'ID' => $row['ID'],
+                'name' => $row['clan'],
+                'tag' => $row['tag'],
+                'desc' => $row['desc'],
+                'clanOwner' => $row['clanOwner'],
+                'color' => $row['color'],
+                'isClosed' => $row['isClosed'],
+                'creationDate' => $row['creationDate']
+            );
+
+            $response[] = $clan;
+        }
+
+        echo json_encode($response);
+    } else {
+        echo "No rows found in the clans table.";
+    }
+} else {
+    echo "Query failed: " . $db->error;
 }
-
-echo json_encode($response);
 ?>
