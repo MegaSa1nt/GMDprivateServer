23bb507d969e39e46c714916b60761978f0ea270
M336 <75208577+M336G@users.noreply.github.com>
2025-06-30 19:28:43 +0200

    Fix stats.php's method
    POST -> GET

--- api/v1/levels/gmd.php
+++ api/v1/levels/gmd.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: GET');
 
 require_once __DIR__."/../../../incl/lib/security.php";
 require_once __DIR__."/../../../incl/lib/exploitPatch.php";
